f0d6602a6be1c6e584e0771bd0e3a9c7b2b57b78
xiaoyao <15558110117@163.com>
2023-04-20 11:15:44 +0800

    fix: add some rules

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -3,4 +3,27 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
   ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-use-before-define': [
+      2,
+      {
+        functions: false,
+        classes: true,
+        typedefs: false,
+      },
+    ],
+    '@typescript-eslint/no-unused-expressions': [
+      2,
+      { allowShortCircuit: true, allowTernary: true },
+    ],
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/consistent-indexed-object-style': 0,
+  },
 };
